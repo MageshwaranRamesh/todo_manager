8a50884d82f14252fedec054dac0fc3b519b0cd7
MageshwaranRamesh <mageshwaran.ramesh@freshworks.com>
2021-03-05 17:54:12 +0530

    bcypt password

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new
+    render "users/index"
+  end
+
+  def adduser
+    # first_name = params[:first_name]
+    # last_name =
+    # email = params[:email]
+    User.create!(first_Name: params[:first_name], last_Name: params[:last_name], email: params[:email], password: params[:password])
+    redirect_to "/"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get "todos", to: "todos#index"
   get "todos/:id", to: "todos#show"
-
+  get "/", to: "home#index"
   post "todos", to: "todos#add"
   delete "todos/:id", to: "todos#delete"
   put "todos/:id", to: "todos#updatechecked"
+  get "/user/new", to: "users#new"
+  post "/users", to: "users#adduser"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
